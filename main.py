29217ad2077a91aa88eb36304334582d03635b8c
Progra <virus4buk@yandex.ru>
2024-12-01 10:38:46 +0800

    Add new color variations

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 import sys
 
 from PyQt6.QtWidgets import QApplication, QWidget
-from PyQt6 import uic
 from PyQt6.QtGui import QPainter, QColor
 from random import randint
+from UI import Ui_Form
 
 
-class Circles(QWidget):
+def make_color() -> QColor:
+    r, g, b = randint(0, 256), randint(0, 256), randint(0, 256)
+    return QColor(r, g, b)
+
+
+class Circles(QWidget, Ui_Form):
     def __init__(self):
         super().__init__()
-        uic.loadUi("Ui.ui", self)
+        self.setupUi(self)
         self.setGeometry(0, 0, 1000, 1000)
         self.need_to_paint = 0
         self.make_circles.clicked.connect(self.do_paint)
@@ -27,9 +32,9 @@ class Circles(QWidget):
         self.update()
 
     def main(self, qp):
-        qp.setBrush(QColor(255, 255, 0))
         count_of_circles = randint(3, 10)
         for i in range(count_of_circles):
+            qp.setBrush(make_color())
             d = randint(20, 50)
             x, y = randint(d, 1000 - d), randint(d, 1000 - d)
             qp.drawEllipse(x, y, d, d)
